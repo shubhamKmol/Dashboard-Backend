3f1024c94328d0154784b15fcc3fe059f5447744
Shubham Kharatmol <shubkmol@gmail.com>
2025-12-18 14:25:57 +0530

    db aadded

--- index.js
+++ index.js
@@ -5,28 +5,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const merchants = [
-    {
-        id: "1",
-        name: "Amazon Seller",
-        status: "ACTIVE",
-        "monthlyVolume": 60000,
-        "chargebackRatio": 1.0,
-        "riskLevel": "low",
-        country: "India"
-    }, {
-        "id": "m7",
-        "name": "Alpha Services",
-        "country": "AU",
-        "status": "paused",
-        "monthlyVolume": 60000,
-        "chargebackRatio": 1.0,
-        "riskLevel": "low"
-    },
-];
+// const merchants = [
+//     {
+//         id: "1",
+//         name: "Amazon Seller",
+//         status: "ACTIVE",
+//         "monthlyVolume": 60000,
+//         "chargebackRatio": 1.0,
+//         "riskLevel": "low",
+//         country: "India"
+//     }, {
+//         "id": "m7",
+//         "name": "Alpha Services",
+//         "country": "AU",
+//         "status": "paused",
+//         "monthlyVolume": 60000,
+//         "chargebackRatio": 1.0,
+//         "riskLevel": "low"
+//     },
+// ];
 
-app.get("/api/merchants", (req, res) => {
-    res.json(merchants);
+// app.get("/api/merchants", (req, res) => {
+//     res.json(merchants);
+// });
+const pool = require("./db");
+
+app.get("/api/merchants", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM merchants");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("DB error:", err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
